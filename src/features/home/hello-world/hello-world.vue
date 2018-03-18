083b547e3ed86933206939f3e68af67fe1c84ea7
Carlos <karlos1337@gmail.com>
2018-03-18 14:49:22 +0100

    Refactor HelloWorld component

---
+++ src/features/home/hello-world/hello-world.vue
@@ -0,0 +1,11 @@
+<template lang="pug" src="./hello-world.pug"></template>
+<style lang="stylus" src="./hello-world.styl" scoped></style>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class HelloWorld extends Vue {
+    @Prop() private msg: string;
+  }
+</script>
